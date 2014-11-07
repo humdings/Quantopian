ef85127e6263e5a57f1e2203e6212bb45dcf9393
David Edwards <dedwards@rohan.sdsu.edu>
2014-11-07 12:31:29 -0800

    Added launchd example

--- order_management/weights_to_orders.py
+++ order_management/weights_to_orders.py
@@ -2,7 +2,7 @@
 import pandas as pd
 
 
-def orders_from_weights(context, W):
+def targets_from_weights(context, W):
     '''
     context: obj
         contains current portfolio state
@@ -10,17 +10,16 @@ def orders_from_weights(context, W):
         target weights vector indexed by sid.
 
     returns: Series
-        order sizes required to achieve target weights.
+        target position sizes required to achieve target weights.
 
     Note: Decimal sizes are allowed here, they will be rounded
           in zipline before they are placed.
     '''
     equity = context.portfolio.portfolio_value
 
-    current_prices = history(1, '1d', 'price').ix[-1]
-    positions = pd.Series({i: context.portfolio.positions[i].amount for i in W.index})
-    targets = (W * equity) / current_prices
-    return targets - positions
+    current_prices = history(1, '1d', 'price').iloc[-1]
+    return (W * equity) / current_prices
+    
 
 
 # These ones are not Quantopian specific, but accomplish the same thing
